3b48d088ed79d275c086e37681d356561c6aa3d6
Denis Granha <denisgranha@gmail.com>
2017-06-01 19:19:19 +0200

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,23 @@ from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
+requirements = [
+    'six',
+    'bitcoin==1.1.42',
+    'celery==3.1.25'
+    'Django==1.10.5',
+    'django-authtools==1.5.0',
+    'django-celery==3.2.1',
+    'django-model-utils==2.6.1',
+    'django-solo==1.1.2',
+    'ethereum==1.6.0',
+    'ethereum-abi-utils==0.4.0',
+    'ethereum-utils==0.2.0',
+    'factory-boy==2.5.2',
+    'web3==3.7.1'
+]
+
 setup(
     name='django-ether-logs',
     version='0.1',
