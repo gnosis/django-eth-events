2feb252c807c21a32e1465bb4dc6bbf68c8f4915
Giacomo <giacomo.licari@gmail.com>
2017-07-12 18:06:59 +0200

    created error_email decorator

--- django_eth_events/tasks.py
+++ django_eth_events/tasks.py
@@ -7,10 +7,43 @@ from django.conf import settings
 from django.core.cache import cache
 from django.core.mail import mail_admins
 from django_eth_events.models import Daemon
+from functools import wraps
 
 logger = get_task_logger(__name__)
 
 oid = 'LOCK'
+
+
+def error_email(func):
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+
+        def send_email(message):
+            # send email
+            mail_admins('[ETH Events Error] ', message)
+
+        try:
+            func(*args, **kwargs)
+        except Exception as err:
+            logger.error(str(err))
+            # get last error block number from cache
+            last_error_block_number = cache.get('last_error_block_number')
+            # get current block number from database
+            current_block_number = Daemon.block_number
+
+            if last_error_block_number and last_error_block_number < current_block_number:
+                send_email(err.message)
+                # save block number into cache
+                cache.set('last_error_block_number', current_block_number)
+            elif not last_error_block_number:
+                # send email
+                send_email(err.message)
+                # save block number into cache
+                cache.set('last_error_block_number', current_block_number)
+
+    return inner
+
 
 @contextmanager
 def cache_lock(lock_id, oid):
@@ -29,30 +62,13 @@ def cache_lock(lock_id, oid):
             cache.delete(lock_id)
 
 
+@error_email
 @shared_task
 def event_listener():
-    try:
-        with cache_lock('eth_events', oid) as acquired:
-            if acquired:
-                bot = EventListener()
-                bot.execute()
-    except Exception as err:
-        logger.error(str(err))
-        # get last error block number from cache
-        last_error_block_number = cache.get('last_error_block_number')
-        # get current block number from database
-        current_block_number = Daemon.block_number
-
-        if last_error_block_number and last_error_block_number == current_block_number:
-            pass
-        else:
-            # send email
-            message = err.message
-            mail_admins('[GnosisDB Celery Error] ', message)
-            # save block number into cache
-            cache.add('last_error_block_number', current_block_number)
-
-
+    with cache_lock('eth_events', oid) as acquired:
+        if acquired:
+            bot = EventListener()
+            bot.execute()
 
 
 
