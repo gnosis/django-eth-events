79050d22208a3f43efbbbd4fb0a2996342fa5cc2
Denís Graña <denisgranha@gmail.com>
2017-10-27 18:17:01 +0200

    Revert "updated web3 provider to http one to be compatible for olympia"
    This reverts commit 03ea45177dd9a9437898fe80e1e41f09ba61109b.

--- django_eth_events/web3_service.py
+++ django_eth_events/web3_service.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from web3 import Web3, HTTPProvider
+from web3 import Web3, RPCProvider
 
 from .singleton import Singleton
 
@@ -7,12 +7,10 @@ from .singleton import Singleton
 class Web3Service(Singleton):
     def __init__(
             self,
-            rpc_provider = HTTPProvider(
-                '{}://{}:{}'.format(
-                    'https' if settings.ETHEREUM_NODE_SSL else 'http',
-                    settings.ETHEREUM_NODE_HOST,
-                    settings.ETHEREUM_NODE_PORT,
-                )
+            rpc_provider = RPCProvider(
+                host=settings.ETHEREUM_NODE_HOST,
+                port=settings.ETHEREUM_NODE_PORT,
+                ssl=settings.ETHEREUM_NODE_SSL
             )):
         super(Web3Service, self).__init__()
         self.web3 = Web3(rpc_provider)
