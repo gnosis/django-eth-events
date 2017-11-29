482eaba3c34159d9919dbce59846c74610b9b584
Denis Granha <denisgranha@gmail.com>
2017-11-29 21:03:20 +0100

    Block model is not singleton

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name='django-eth-events',
-    version='1.0.5',
+    version='1.0.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
