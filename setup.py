a86fbd2aaa053b4cbcc0644323ec0c1715d9f7d8
Denis Granha <denisgranha@gmail.com>
2017-11-30 11:13:31 +0100

    pump version 1.0.12

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 
 setup(
     name='django-eth-events',
-    version='1.0.11',
+    version='1.0.12',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
