1c2f1516aa322ffa376ce318872ea2e79c4b7e9d
Uxio Fuentefria <uxio@gnosis.pm>
2018-01-25 13:50:38 +0100

    Don't use mutable dictionary as default param

--- django_eth_events/web3_service.py
+++ django_eth_events/web3_service.py
@@ -7,10 +7,12 @@ from .singleton import Singleton
 class Web3Service(Singleton):
     def __init__(
             self,
+            rpc_provider=None):
+        if not rpc_provider:
             rpc_provider = RPCProvider(
                 host=settings.ETHEREUM_NODE_HOST,
                 port=settings.ETHEREUM_NODE_PORT,
                 ssl=settings.ETHEREUM_NODE_SSL
-            )):
+            )
         super(Web3Service, self).__init__()
         self.web3 = Web3(rpc_provider)
