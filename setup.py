5ae1660eee681ab2236da9b6af861a24fc11991b
Uxio <uxio@gnosis.pm>
2018-01-29 12:05:16 +0100

    Set version 1.0.29
    - Don't read requirements from setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,23 @@ from setuptools import find_packages, setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-requirements = []
 
-with open('requirements.txt', 'r') as f:
-    requirements = [x for x in f.read().split('\n') if x != '']
+requirements = [
+    "six==1.11.0",
+    "bitcoin==1.1.42",
+    "celery==4.1.0",
+    "Django==1.11",
+    "django-solo==1.1.2",
+    "ethereum==1.6.1",
+    "ethereum-abi-utils==0.4.0",
+    "ethereum-utils==0.2.0",
+    "kombu==4.1.0",
+    "web3[tester]==3.7.1"
+]
 
 setup(
     name='django-eth-events',
-    version='1.0.28',
+    version='1.0.29',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
