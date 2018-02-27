3365c325065bc7cf2b0fa0585fc81272053ddc2f
Uxio Fuentefria <uxio@gnosis.pm>
2018-02-27 17:44:41 +0100

    Add support for Ethereum IPC

--- django_eth_events/web3_service.py
+++ django_eth_events/web3_service.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from web3 import RPCProvider, Web3
+from web3 import IPCProvider, RPCProvider, Web3
 
 
 class Web3Service(object):
@@ -11,15 +11,21 @@ class Web3Service(object):
 
     class __Web3Service:
         web3 = None
-        default_provider = RPCProvider
+        default_provider_class = RPCProvider
 
         def __init__(self, provider=None):
             if not provider:
-                provider = self.default_provider(
-                    host=settings.ETHEREUM_NODE_HOST,
-                    port=settings.ETHEREUM_NODE_PORT,
-                    ssl=settings.ETHEREUM_NODE_SSL
-                )
+                if hasattr(settings, 'ETHEREUM_IPC_PATH'):
+                    self.default_provider_class = IPCProvider
+                    provider = self.default_provider_class(
+                        ipc_path=settings.ETHEREUM_IPC_PATH
+                    )
+                else:
+                    provider = self.default_provider_class(
+                        host=settings.ETHEREUM_NODE_HOST,
+                        port=settings.ETHEREUM_NODE_PORT,
+                        ssl=settings.ETHEREUM_NODE_SSL
+                    )
 
             self.web3 = Web3(provider)
 
@@ -30,7 +36,7 @@ class Web3Service(object):
                                          Web3Service.instance.web3.providers[0].__class__):
             Web3Service.instance = Web3Service.__Web3Service(provider)
         elif not provider and not isinstance(Web3Service.instance.web3.providers[0],
-                                             Web3Service.instance.default_provider):
+                                             Web3Service.instance.default_provider_class):
             Web3Service.instance = Web3Service.__Web3Service(provider)
         return Web3Service.instance
 
