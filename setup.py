95ee0e4a93208c1ca5168b22879a9d16e2a4e21e
Uxio Fuentefria <uxio@gnosis.pm>
2018-02-28 16:53:12 +0100

    Set version 1.0.38

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name='django-eth-events',
-    version='1.0.37',
+    version='1.0.38',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
