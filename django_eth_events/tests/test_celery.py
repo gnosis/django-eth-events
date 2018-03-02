54a50fd1b108c9cd5492ac60ea3258a0f8c4de59
Uxio Fuentefria <uxio@gnosis.pm>
2018-03-02 14:03:02 +0100

    Remove unused imports

--- django_eth_events/tests/test_celery.py
+++ django_eth_events/tests/test_celery.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 from time import sleep
 
 from django.conf import settings
--- django_eth_events/tests/test_event_listener_exec.py
+++ django_eth_events/tests/test_event_listener_exec.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
 from json import loads
 
 from django.test import TestCase
