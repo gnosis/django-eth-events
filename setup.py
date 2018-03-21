84eb0f5ca99e5beb52847c6310934de40445b650
Uxio Fuentefria <uxio@gnosis.pm>
2018-03-21 11:04:39 +0100

    Set version 2.1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
 
 setup(
     name='django-eth-events',
-    version='2.0.9',
+    version='2.1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
