018c26c63645c49790c95d123412a3c479df6b3b
Uxio Fuentefria <uxio@gnosis.pm>
2018-04-02 17:43:19 +0200

    Store version in python module

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 import os
+from distutils.util import convert_path
 
 from setuptools import find_packages, setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def read_version():
+    main_ns = {}
+    ver_path = convert_path('django_eth_events/version.py')
+    with open(ver_path) as ver_file:
+        exec(ver_file.read(), main_ns)
+    return main_ns['__version__']
+
+version = read_version()
 
 
 requirements = [
@@ -19,7 +30,7 @@ requirements = [
 
 setup(
     name='django-eth-events',
-    version='2.1.0',
+    version=version,
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
